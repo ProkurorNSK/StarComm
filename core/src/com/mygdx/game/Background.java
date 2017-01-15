2dc59362ff9a459ab0075c0810d9ae4a832ed058
ProkurorNSK <proka@ngs.ru>
2017-01-16 02:00:40 +0700

    Start

---
+++ core/src/com/mygdx/game/Background.java
@@ -0,0 +1,43 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Vector;
+
+class Background {
+
+    class Star {
+        private Vector2 position;
+        private float speed;
+
+        public Star() {
+            position = new Vector2((float) Math.random() * 1280, (float) Math.random() * 1280);
+            speed = 5.0f;
+        }
+
+        public void update() {
+            position.x -=  speed;
+            if (position.x < 0) {
+                position.x = 1280;
+                position.y = (float) Math.random() * 720;
+            }
+        }
+    }
+
+    private Texture texture;
+    private Texture textureStar;
+    private Star[] stars;
+    private final int STARS_COUNT = 200;
+
+    Background() {
+        texture = new Texture("staticback.jpg");
+        textureStar = new Texture("star12.tga");
+        stars = new Star[STARS_COUNT];
+    }
+
+    void render(SpriteBatch batch) {
+        batch.draw(texture, 0, 0);
+    }
+}
---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private Background background;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Background();
+	}
+
+	@Override
+	public void render () {
+		update();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		background.render(batch);
+		batch.end();
+	}
+
+	private void update() {
+
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
